68c6f4d841fa9daa5cfb978995e18be98bc96ab0
louismineo <louismineo@gmail.com>
2021-08-18 02:49:45 +0000

    Added my project

---
+++ script.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--fullscreen")
+
+
+PATH = r"/usr/local/bin/chromedriver"
+
+
+print("Running webdriver for chrome")
+driver = webdriver.Chrome(PATH,options=chrome_options)
+
+driver.get("https://dayuse.sg")
+print(driver.title)
